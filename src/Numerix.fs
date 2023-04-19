7370f3b87de9a26d86c716d09e955afd4b5252ae
Gary Becks <42525743+g5becks@users.noreply.github.com>
2023-04-19 12:30:04 -0400

    refactor(Numerix): fix Numerix.stdDev function
    fix error in the Numerix.stdDev function and add tests

--- src/Numerix.fs
+++ src/Numerix.fs
@@ -2,50 +2,43 @@ module Incremental.Indicators.Numerix
 
 open System
 
-// Check if the array contains infinity as the division will fail
-let containsInfinity (list: double[]) =
-    list |> Array.contains infinity
-    || list |> Array.contains -infinity
-    || list |> Array.contains +infinity
+let (|IsNull|_|) value =
+    if obj.ReferenceEquals(value, null) then None else Some()
 
 // check for nulls, empty arrays, and existing of NaN or Infinity
 let dataIsValid (list: double[]) =
     match list with
     | [||] -> Error("Values cannot be empty")
     | null -> Error("Values cannot be null")
-    | list when containsInfinity list -> Error("Infinity is not allowed")
-    | list when Array.exists Double.IsNaN list -> Error("NaN is not allowed")
     | _ -> Ok(list)
 
 // calculate the mean
 let mean (values: double[]) =
     match dataIsValid values with
-    | Error _ -> double Double.NaN
     | Ok _ -> Array.sum values / double values.Length
+    | Error _ -> Double.NaN
+
 
 // Standard Deviation
 let stdDev (values: double[]) =
-    let calc =
+    match dataIsValid values with
+    | Error(A) -> Error(A)
+    | Ok _ ->
         let n = double values.Length
         let avg = mean values
         let squaredDiffs = Array.map (fun v -> (v - avg) ** 2.0) values
         Ok(sqrt (Array.sum squaredDiffs / n))
 
-    match dataIsValid values with
-    | Error(A) -> Error(A)
-    | Ok _ -> calc
-
 // SLOPE of BEST FIT LINE
 let slope (x: double[]) (y: double[]) =
-    let calc =
-        let meanX = mean x
-        let meanY = mean y
-        let numerator = Array.map2 (fun x y -> (x - meanX) * (y - meanY)) x y |> Array.sum
-        let denominator = Array.map (fun x -> (x - meanX) ** 2.0) x |> Array.sum
-        Ok(numerator / denominator)
     // validate params
     match dataIsValid x, dataIsValid y with
     | Error(A), _ -> Error(A)
     | _, Error(A) -> Error(A)
     | Ok(x), Ok(y) when x.Length <> y.Length -> Error("Slope x and y must be the same size")
-    | _ -> calc
+    | _ ->
+        let meanX = mean x
+        let meanY = mean y
+        let numerator = Array.map2 (fun x y -> (x - meanX) * (y - meanY)) x y |> Array.sum
+        let denominator = Array.map (fun x -> (x - meanX) ** 2.0) x |> Array.sum
+        Ok(numerator / denominator)
--- tests/Numerix.fs
+++ tests/Numerix.fs
@@ -3,35 +3,75 @@ module Incremental.Indicators.Tests.Numerix
 open Incremental.Indicators.Numerix
 open Expecto
 open System
-open FsCheck
 
-let config =
-    { FsCheckConfig.defaultConfig with
-        maxTest = 10000 }
+let isNaN (num: double) = Double.IsNaN num
+
+let isResultNaN (res: Result<double, string>) =
+    match res with
+    | Ok(num) when isNaN num -> true
+    | _ -> false
+
+let getRes res =
+    match res with
+    | Ok(num) -> num
+    | Error _ -> 0.0
 
 [<Tests>]
-let tests =
+let meanTests =
     testList
-        "Numerix Tests"
-        [ testProperty "The mean of an array equals the sum divided by the length"
-          <| fun (values: double[]) ->
-              if
-                  Array.isEmpty values
-                  || Array.exists Double.IsNaN values
-                  || containsInfinity values
-              then
-                  true
-              else
-                  let expectedMean = Array.sum values / double values.Length
-                  let actualMean = mean values
-                  expectedMean = actualMean
+        "Numerix.mean Tests"
+        [ test "Empty list returns NaN" {
+              let result = mean [||]
+              let expected = isNaN result
+              Expect.isTrue expected "should be NaN"
+          }
+          test "Null list returns NaN" {
+              let result = mean null
+              let expected = isNaN result
+              Expect.isTrue expected "should be NaN"
+          }
+          test "Calculates mean of values" {
+              let values = [| 1.0; 2.0; 3.0; 4.0; 5.0 |]
+              let expected = 3.0
+              let result = mean values
+              Expect.equal result expected ""
+          } ] //
 
-          testProperty "Standard deviation is always positive"
-          <| fun values ->
-              let result = stdDev values
+[<Tests>]
+let stdDevTests = // tolerance ]
+    testList
+        "Numerix.stdDev Tests"
+        [ testCase "Empty Array"
+          <| fun _ ->
+              let values = [||]
+              let actual = stdDev values
+              Expect.isError actual "should be an error"
 
-              match result with
-              | Error _ -> true
-              | Ok x -> x >= 0.0
+          testCase "Null Values"
+          <| fun _ ->
+              let values = null
+              let actual = stdDev values
+              Expect.isError actual "should be an error"
 
-          ]
+          testCase "Contains NaN"
+          <| fun _ ->
+              let values = [| 1.0; Double.NaN; 3.0; 4.0 |]
+              let actual = stdDev values
+              let expected = isResultNaN actual
+              Expect.isTrue expected "should be NaN"
+
+          testCase "Contains Infinity"
+          <| fun _ ->
+              let values = [| 1.0; Double.PositiveInfinity; 3.0; 4.0 |]
+              let actual = stdDev values
+              let expected = isResultNaN actual
+              Expect.isTrue expected "should be NaN"
+
+          testCase "Calculate stdDev for valid input"
+          <| fun _ ->
+              let values = [| 1.0; 2.0; 3.0; 4.0; 5.0 |]
+              let expected = 1.41421356237309
+              let actual = stdDev values
+              Expect.isOk actual "should be Ok"
+              let actual = stdDev values |> getRes
+              Expect.floatClose Accuracy.high actual expected "should be within accuracy" ]
