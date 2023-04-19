fefbdb147b015d2c86e11cd27f8f94639d81707d
Gary Becks <42525743+g5becks@users.noreply.github.com>
2023-04-19 13:20:36 -0400

    feat(Types): add toTimeSpan method
    added toTimeSpan method to TimeFrame type to convert a TimeFrame to a TimeSpan

--- src/Types.fs
+++ src/Types.fs
@@ -1,5 +1,8 @@
 module Incremental.Indicators.Types
 
+open System
+
+[<RequireQualifiedAccess>]
 type CandlePart =
     | Open
     | High
@@ -12,6 +15,7 @@ type CandlePart =
     | OHL3
     | OHLC4
 
+[<RequireQualifiedAccess>]
 type EndType =
     | Close
     | HighLow
@@ -26,6 +30,7 @@ type Match =
     | BearSignal = -100
     | BearConfirmed = -200
 
+[<RequireQualifiedAccess>]
 type MaType =
     | ALMA
     | DEMA
@@ -39,6 +44,7 @@ type MaType =
     | TEMA
     | WMA
 
+[<RequireQualifiedAccess>]
 type TimeFrame =
     | ThreeWeeks
     | TwoWeeks
@@ -50,13 +56,18 @@ type TimeFrame =
     | FiveDays
     | ThreeDays
     | TwoDays
-    | Day
+    | OneDay
+    | TwentyHours
+    | EightTeenHours
+    | SixteenHours
+    | FourteenHours
     | TwelveHours
     | EightHours
+    | TenHours
     | SixHours
-    | FourHour
-    | ThreeHour
-    | TwoHour
+    | FourHours
+    | ThreeHours
+    | TwoHours
     | OneHour
     | ThreeHundredNinetyMin
     | TwoHundredSixtyMin
@@ -70,3 +81,41 @@ type TimeFrame =
     | FiveMin
     | ThreeMin
     | OneMin
+
+    member self.toTimeSpan() =
+        match self with
+        | ThreeWeeks -> TimeSpan.FromDays 21
+        | TwoWeeks -> TimeSpan.FromDays 14
+        | Week -> TimeSpan.FromDays 7
+        | ThirtyDays -> TimeSpan.FromDays 30
+        | TwentyDays -> TimeSpan.FromDays 20
+        | FifteenDays -> TimeSpan.FromDays 15
+        | TenDays -> TimeSpan.FromDays 10
+        | FiveDays -> TimeSpan.FromDays 5
+        | ThreeDays -> TimeSpan.FromDays 3
+        | TwoDays -> TimeSpan.FromDays 2
+        | OneDay -> TimeSpan.FromDays 1
+        | TwentyHours -> TimeSpan.FromHours 20
+        | EightTeenHours -> TimeSpan.FromHours 18
+        | SixteenHours -> TimeSpan.FromHours 16
+        | FourteenHours -> TimeSpan.FromHours 14
+        | TwelveHours -> TimeSpan.FromHours 12
+        | TenHours -> TimeSpan.FromHours 10
+        | EightHours -> TimeSpan.FromHours 8
+        | SixHours -> TimeSpan.FromHours 6
+        | FourHours -> TimeSpan.FromHours 4
+        | ThreeHours -> TimeSpan.FromHours 3
+        | TwoHours -> TimeSpan.FromHours 2
+        | OneHour -> TimeSpan.FromHours 1
+        | ThreeHundredNinetyMin -> TimeSpan.FromMinutes 390
+        | TwoHundredSixtyMin -> TimeSpan.FromMinutes 260
+        | OneHundredThirtyMin -> TimeSpan.FromMinutes 130
+        | SixtyFiveMin -> TimeSpan.FromMinutes 65
+        | FortyFiveMin -> TimeSpan.FromMinutes 45
+        | ThirtyMin -> TimeSpan.FromMinutes 30
+        | TwentyFourMin -> TimeSpan.FromMinutes 24
+        | FifteenMin -> TimeSpan.FromMinutes 15
+        | TwelveMin -> TimeSpan.FromMinutes 12
+        | FiveMin -> TimeSpan.FromMinutes 5
+        | ThreeMin -> TimeSpan.FromMinutes 3
+        | OneMin -> TimeSpan.FromMinutes 1
