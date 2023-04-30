c351e8d4849170835cac298c4f8310e9b7515bba
Gary Becks <42525743+g5becks@users.noreply.github.com>
2023-04-29 20:57:22 -0400

    fix(RSI): fix rsi
    update rsi function

--- src/RSI.fs
+++ src/RSI.fs
@@ -2,103 +2,43 @@ module Incremental.Indicators.RSI
 
 open FSharp.Data.Adaptive
 open System
-
-type Quote =
-    { Date: DateTime
-      Open: decimal
-      High: decimal
-      Low: decimal
-      Close: decimal
-      Volume: decimal }
-
-    static member Empty =
-        { Date = DateTime.MaxValue
-          Open = 0m
-          Close = 0m
-          High = 0m
-          Low = 0m
-          Volume = 0m }
-
-    static member IsEmpty q = q.Date = DateTime.MaxValue
-
-
-type internal QuoteD =
-    { Date: DateTime
-      Open: double
-      High: double
-      Low: double
-      Close: double
-      Volume: double }
-
-    static member Empty =
-        { Date = DateTime.MaxValue
-          Open = 0.0
-          Close = 0.0
-          High = 0.0
-          Low = 0.0
-          Volume = 0.0 }
-
-    static member IsEmpty q = q.Date = DateTime.MaxValue
-
-// convert to quotes in double precision
-// QuoteD alist sorted by date
-let internal toQuoteDList (quotes: Quote alist) =
-    quotes
-    |> AList.map (fun x ->
-        { Date = x.Date
-          Open = double x.Open
-          High = double x.High
-          Low = double x.Low
-          Close = double x.Close
-          Volume = double x.Volume })
-    |> AList.sortBy (fun x -> x.Date)
-
-let internal getVal offset defaultVal list =
-    aval {
-        let! item = AList.sub offset 1 list |> AList.tryFirst
-        return Option.defaultValue defaultVal item
-    }
+open Util
 
 type RsiResult = { Value: double; Date: DateTime }
 
-let calculateRSI (quotes: Quote alist) (period: int) =
-    let newQuotes = toQuoteDList quotes
+let calculateRSI (lookBack: int) (quotes: Quotes) =
+    let mutable gain: double = 0.0
+    let mutable loss: double = 0.0
+    let mutable rs: double = 0.0
+    let mutable rsi: double = 0.0
+    let mutable count = 0
+    let mutable index = 0
+    let mutable buffer = Array.create lookBack (double 0.0, double 0.0)
 
     alist {
-        let! len = AList.count newQuotes
-        let mutable gain: double = 0.0
-        let mutable loss: double = 0.0
-        let mutable rs: double = 0.0
-        let mutable rsi: double = 0.0
-
-        for i in 1..period do
-            let! currentQuote = getVal i QuoteD.Empty newQuotes
-            let! previousQuote = getVal (i - 1) QuoteD.Empty newQuotes
-            let diff = currentQuote.Close - previousQuote.Close
+        for quote in quotes.DoublePrecis do
+            count <- count + 1
+            let! previousQuote = getVal (count - 1) QuoteD.Empty quotes.DoublePrecis
+            let diff = quote.Close - previousQuote.Close
 
             if diff > 0.0 then
                 gain <- gain + diff
+                let prevGain, prevLoss = buffer[index]
+                loss <- prevLoss + max 0.0 (prevGain - (prevGain + diff))
+                buffer[index] <- (diff, prevLoss)
             else
-                loss <- loss + abs (diff)
+                loss <- loss + abs diff
+                let prevGain, prevLoss = buffer[index]
+                gain <- prevGain + max 0.0 (prevLoss - (prevLoss + abs diff) / double lookBack)
+                buffer[index] <- (prevGain, abs diff)
 
-        rs <- gain / loss
-        rsi <- 100.0 - (100.0 / (1.0 + rs))
+            index <- (index + 1) % lookBack
 
-        for i in (period + 1) .. len - 1 do
-            let! currentQuote = getVal i QuoteD.Empty newQuotes
-            let! previousQuote = getVal (i - 1) QuoteD.Empty newQuotes
-            let diff = currentQuote.Close - previousQuote.Close
-
-            if diff > 0.0 then
-                gain <- gain + diff - (gain / double period)
-                loss <- loss - (loss / double period)
+            if count > lookBack then
+                rs <- gain / loss
+                rsi <- 100.0 - (100.0 / (1.0 + rs))
             else
-                loss <- loss + abs (diff) - (loss / double period)
-                gain <- gain - (gain / double period)
+                rsi <- Double.NaN
 
-            rs <- gain / loss
-            rsi <- 100.0 - (100.0 / (1.0 + rs))
-
-        rsi
-
+            yield { Value = rsi; Date = quote.Date }
     }
