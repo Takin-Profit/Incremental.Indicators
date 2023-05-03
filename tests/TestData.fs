8e3a3850be58ea411cf50c5b84e5ac4135c3d3ac
Gary Becks <42525743+g5becks@users.noreply.github.com>
2023-05-03 16:48:16 -0400

    test(Quotes): getQuotes helper
    added helper function to parse quotes from a csv file.

--- tests/TestData.fs
+++ tests/TestData.fs
@@ -1,14 +1,15 @@
-module Incremental.Indicators.Tests.CsvReader
+module Incremental.Indicators.Tests.TestData
 
 open Incremental.Indicators
+open FSharp.Data.Adaptive
 open FSharp.Data
 open System
 
 
-let getCsvRow = CsvFile.Load(__SOURCE_DIRECTORY__ + "/../data/MSFT.csv").Cache()
+let private csvFile name =
+    CsvFile.Load(__SOURCE_DIRECTORY__ + $"Data/{name}").Cache()
 
-
-let quoteFromCsv (row: CsvRow) =
+let private quoteFromCsv (row: CsvRow) =
     if row.Columns |> Array.contains "" then
         None
     else
@@ -30,3 +31,23 @@ let quoteFromCsv (row: CsvRow) =
               Low = row.GetColumn "low" |> decimal
               Close = row.GetColumn "close" |> decimal
               Volume = row.GetColumn "volume" |> decimal })
+
+
+let private getQuotes file days =
+    let file = csvFile file
+
+    let quotes =
+        seq {
+            for row in file.Rows do
+                yield quoteFromCsv row
+        }
+
+    if quotes |> Seq.exists Option.isNone then
+        None
+    else
+        quotes
+        |> Seq.map (fun t -> Option.defaultValue Quote.Empty t)
+        |> AList.ofSeq
+        |> Some
+        |> Option.map (AList.take days)
+        |> Option.map (AList.sortByDescending (fun t -> t.Date))
