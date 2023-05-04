38c0168aa993631e260047cc788a81e9041e942b
Gary Becks <42525743+g5becks@users.noreply.github.com>
2023-05-03 21:03:12 -0400

    test(Quotes): getTupleNan function

--- tests/TestData.fs
+++ tests/TestData.fs
@@ -51,3 +51,77 @@ let private getQuotes file days =
         |> Seq.sortByDescending (fun t -> t.Date)
         |> AList.ofSeq
         |> Some
+
+// DEFAULT: S&P 500 ~2 years of daily data
+let getDefault days = getQuotes "default.csv" days
+
+let getZeroes days = getQuotes "zeroes.csv" days
+
+let getBad days = getQuotes "bad.csv" days
+
+let getTooBig days = getQuotes "toobig.csv" days
+
+let getMax days = getQuotes "toobig.csv" days
+// bitcoin data
+let getBitcoin days = getQuotes "bitcoin.csv" days
+// COMPARE DATA ~2 years of TSLA data (matches default time)
+let getCompare days = getQuotes "compare.csv" days
+// INTRADAY DATA
+let getIntraDay days = getQuotes "intraday.csv" days
+// LONGISH DATA ~20 years of S&P 500 daily data
+let getLongish days = getQuotes "longish.csv" days
+// LONGEST DATA ~62 years of S&P 500 daily data
+let getLongest days = getQuotes "longest.csv" days
+// Penny Data
+let getPenny days = getQuotes "penny.csv" days
+// MISMATCH DATA is in incorrect sequence
+let getMismatch days = getQuotes "mismatch.csv" days
+// SPX, 30 years, daily
+let getSpx days = getQuotes "spx.csv" days
+// MSFT, 30 years, daily
+let getMsft days = getQuotes "msft.csv" days
+// BTCUSD, 69288 records, 15-minute bars
+let getBtcUsdNan days = getQuotes "btcusd15x69k.csv" days
+
+let getTupleNan =
+    let timeFactor = double 10000000
+    let mutable date = DateTime.UtcNow
+
+    alist {
+
+        for _ in 0 .. 25 - 1 do
+            date <- date.AddDays 1
+            let value = double (date.ToFileTime()) / timeFactor
+            yield (date, value)
+
+        for _ in 0 .. 25 - 1 do
+            date <- date.AddDays 1
+            let value = double (- date.ToFileTime()) / timeFactor
+            yield (date, value)
+
+        for _ in 0 .. 25 - 1 do
+            date <- date.AddDays 1
+            let value = double 0
+            yield (date, value)
+
+        for _ in 0 .. 25 - 1 do
+            date <- date.AddDays 1
+            let value = double -10
+            yield (date, value)
+
+        for _ in 0 .. 25 - 1 do
+            date <- date.AddDays 1
+            let value = double 10
+            yield (date, value)
+
+        for _ in 0 .. 25 - 1 do
+            date <- date.AddDays 1
+            let value = Double.NaN
+            yield (date, value)
+
+        for _ in 0 .. 25 - 1 do
+            date <- date.AddDays 1
+            let value = double (date.ToFileTime()) / timeFactor
+            yield (date, value)
+
+    }
