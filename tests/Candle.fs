c509b3301fd59a55dd000a6cb42a11242d1f351d
Gary Becks <42525743+g5becks@users.noreply.github.com>
2023-05-04 23:23:43 -0400

    test(Candle): candle tests
    more candle module tests.

--- tests/Candle.fs
+++ tests/Candle.fs
@@ -30,4 +30,43 @@ let candleTests =
           <| fun _ ->
               let lastDate = DateTime.ParseExact("12/31/2018", "MM/dd/yyyy", CultureInfo("en-US"))
 
-              Expect.equal candles[candles.Count - 1].Date lastDate "dates should match" ]
+              Expect.equal candles[candles.Count - 1].Date lastDate "dates should match"
+
+          testCase "spot candle date should match"
+          <| fun _ ->
+              let spotDate = DateTime.ParseExact("03/16/2017", "MM/dd/yyyy", CultureInfo("en-US"))
+
+              Expect.equal candles[50].Date spotDate "dates should match"
+
+          ]
+
+[<Tests>]
+let moreCandleTests =
+    let quotes = TestData.getDefault 502 |> Option.defaultValue AList.empty
+
+    let candles = Candle.toCandleResults quotes |> AList.force
+
+    testList
+        "candle tests extended"
+        [ testCase "candles list should have proper quantities"
+          <| fun _ ->
+              let result = candles.Count
+              Expect.equal result 502 "should be 502"
+
+          testCase "candles should have correct values"
+          <| fun _ ->
+              let res0 = candles[0]
+
+              Expect.equal res0.Candle.Close 212.8m "close price should match"
+              Expect.equal res0.Candle.Size 1.83m "candle size be correct"
+              Expect.equal res0.Candle.Body 0.19m "candle body should match"
+              Expect.equal res0.Candle.UpperWick 0.55m "upperWick should be correct"
+              Expect.equal res0.Candle.LowerWick 1.09m "lowerWick should be correct"
+              Expect.equal (Math.Round(res0.Candle.BodyPct, 5)) 0.10383 "body percent should be correct"
+              Expect.equal (Math.Round(res0.Candle.UpperWickPct, 5)) 0.30055 "upperWick percent should be correct"
+              Expect.equal (Math.Round(res0.Candle.LowerWickPct, 5)) 0.59563 "lowerWick percent should be correct"
+              Expect.isTrue res0.Candle.IsBullish "should calculate bullishness correctly"
+              Expect.isFalse res0.Candle.IsBearish "should calculate bearishness correctly"
+
+
+          ]
